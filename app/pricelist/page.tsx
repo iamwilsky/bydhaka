0d19c594ff25eddc4b45a3a17e084173b01cee09
iamwilsky <willyarsal@gmail.com>
2026-01-06 10:22:45 +0700

    update: add og image for pricelist

--- app/pricelist/page.tsx
+++ app/pricelist/page.tsx
@@ -10,6 +10,9 @@ export const metadata: Metadata = {
     title: 'Daftar Harga Mobil BYD Terbaru - Pricelist OTR Jakarta',
     description: 'Cek Pricelist BYD terbaru. Daftar harga OTR BYD Sealion 7, Seal, Atto 3, dan Dolphin. Dapatkan info promo, diskon, dan simulasi kredit DP ringan di BYD Jakarta.',
     keywords: ['Harga BYD', 'Pricelist BYD', 'Harga BYD Seal', 'Harga BYD Atto 3', 'Harga BYD Dolphin', 'BYD Jakarta'],
+    openGraph: {
+        images: ['/images/og-image-pricelist.webp'],
+    },
 }
 
 export default async function PriceListPage() {
