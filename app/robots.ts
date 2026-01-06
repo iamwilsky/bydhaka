cdd4f0ad7ff1e1439a47a5883bc161363d544849
iamwilsky <willyarsal@gmail.com>
2026-01-06 11:44:46 +0700

    chore: update sitemap and robots domain

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: '/admin',
         },
-        sitemap: 'https://bydlentengagung.com/sitemap.xml',
+        sitemap: 'https://bydjakpus.com/sitemap.xml',
     }
 }
