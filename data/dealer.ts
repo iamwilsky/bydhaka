05165663b54ceac5e368e00be5558903655cf059
iamwilsky <willyarsal@gmail.com>
2026-03-03 09:14:34 +0700

    Update Maps URL for BYD Haka Kebon Jeruk

--- data/dealer.ts
+++ data/dealer.ts
@@ -1,10 +1,10 @@
 export const dealerData = {
-  salesName: "Gerry",
-  salesPhone: "628131399982", // Format 62 for WhatsApp link
-  displayPhone: "0813-1399-9982", // Format for display
+  salesName: "Harry",
+  salesPhone: "6287852107181", // Format 62 for WhatsApp link
+  displayPhone: "0878-5210-7181", // Format for display
   dealerName: "BYD Jakarta",
-  address: "Jl. Kramat Raya No.158, Kenari, Kec. Senen, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10320",
-  domain: "bydjakpus.com",
-  email: "sales@bydjakpus.com", // Generic placeholder or specific if available
-  mapsUrl: "https://maps.google.com/?q=Jl.+Kramat+Raya+No.158,+Kenari,+Kec.+Senen,+Kota+Jakarta+Pusat" // Placeholder maps link
+  address: "Jl. Panjang No.16, Kedoya, Kec. Kb. Jeruk, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11520",
+  domain: "bydhaka.com",
+  email: "sales@bydhaka.com", // Generic placeholder or specific if available
+  mapsUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.7663042876243!2d106.76087127498987!3d-6.16204479382514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f700385965b7%3A0x4f22f15319bb82c0!2sBYD%20Haka%20Kebon%20Jeruk!5e0!3m2!1sen!2sid!4v1772503886869!5m2!1sen!2sid"
 };
